5cee625813c4a9eb14e9d6ad0a0fa1c2f5f0848a
Jonas Hellgren <jonas.hellgren@volvo.com>
2021-06-28 16:35:44 +0200

    Base version working

---
+++ src/main/java/udemycourse/nn2refined/twolayernetwork/Parameters.java
@@ -0,0 +1,11 @@
+package udemycourse.nn2refined.twolayernetwork;
+
+public class Parameters {
+    private Parameters() {
+
+    }
+
+    public static final float LEARNING_RATE = 0.3f;
+    public static final float MOMENTUM = 0.6f;
+    public static final int ITERATIONS = 100000;
+}
