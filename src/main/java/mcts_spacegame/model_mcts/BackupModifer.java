e9e22f79271c96ea9963a820e8913b9c2fc9c10b
Jonas Hellgren <jonas.hellgren@volvo.com>
2022-11-28 08:16:12 +0100

    BackupModifer - backupNormalFromTreeSteps

--- src/main/java/mcts_spacegame/model_mcts/BackupModifer.java
+++ src/main/java/mcts_spacegame/model_mcts/BackupModifer.java
@@ -1,40 +1,58 @@
 package mcts_spacegame.model_mcts;
 
+import black_jack.models_episode.EpisodeItem;
+import mcts_spacegame.enums.Action;
 import mcts_spacegame.environment.StepReturn;
 import mcts_spacegame.models_mcts_nodes.NodeInterface;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /***
  *    Fail states normally gives big negative rewards, to avoid destructive backup, measures below are taken
  *
  *   end node in path:
- *   1) non fail and terminal => normal backup
+ *   1) terminal-non fail => normal backup
  *   2) non terminal => normal backup
- *   3) fail and terminal => defensive backup (simulations not applicable for case)
+ *   3) terminal-non fail => defensive backup (simulations not applicable for case)
  *
  *    normal backup = backup all nodes in path
- *    defensive backup = backup parent of end node AND
- *    every node in path who's children's all are fail and terminal
+ *    defensive backup = backup parent of end node AND set grand parent as terminal if its
+ *    all children's all are fail-terminal
  *
  *   a single simulation:
- *   1) non fail and terminal => normal backup
- *   2) fail and terminal =>  defensive backup
+ *   1) terminal-non fail => normal backup
+ *   2) terminal-fail =>  defensive backup
  *
  */
 
 public class BackupModifer {
 
+    private static final int DISCOUNT_FACTOR = 1;
+
+    List<Action> actions;
     List<NodeInterface> nodesFromRootToSelected;
     List<StepReturn> treeSteps;
     List<List<StepReturn>> simulationResults;
 
-    public BackupModifer(List<NodeInterface> nodesFromRootToSelected,
+    double discountFactor;
+
+
+    public BackupModifer(List<Action> actions,
+                         List<NodeInterface> nodesFromRootToSelected,
                          List<StepReturn> treeSteps,
                          List<List<StepReturn>> simulationResults) {
+        this.actions=actions;
         this.nodesFromRootToSelected = nodesFromRootToSelected;
         this.treeSteps = treeSteps;
         this.simulationResults = simulationResults;
+
+        if (actions.size()!= nodesFromRootToSelected.size() || actions.size()!= treeSteps.size())  {
+            throw new IllegalArgumentException("Non equal sizes of input lists");
+        }
+
+        discountFactor= DISCOUNT_FACTOR;
     }
 
 
@@ -46,10 +64,31 @@ public class BackupModifer {
         NodeInterface endNode=nodesFromRootToSelected.get(lengthPath-1);
 
         if (endNode.isTerminalNoFail() || endNode.isNotTerminal())  {
-            //backupNormalFromTreeSteps();
+            backupNormalFromTreeSteps();
         } else
         {
            // backupDefensiveFromTreeSteps();
+        }
+    }
+
+    private void backupNormalFromTreeSteps()  {
+        double G=0;
+        List<Double> GList=new ArrayList<>();
+        for (int i = treeSteps.size()-1; i >=0 ; i--) {
+            StepReturn ei= treeSteps.get(i);
+            G=G+ discountFactor *ei.reward;
+            GList.add(G);
+            }
+        Collections.reverse(GList);
+        System.out.println("GList = " + GList);
+
+
+        for (NodeInterface node:nodesFromRootToSelected)  {
+            Action action=actions.get(nodesFromRootToSelected.indexOf(node));
+            G= GList.get(nodesFromRootToSelected.indexOf(node));
+            node.increaseNofVisits();
+            node.increaseNofActionSelections(action);
+            node.updateActionValue(G,action);
         }
 
 
---
+++ src/test/java/mcts_spacegame/TestBackupModifer.java
@@ -0,0 +1,90 @@
+package mcts_spacegame;
+
+import mcts_spacegame.enums.Action;
+import mcts_spacegame.environment.Environment;
+import mcts_spacegame.environment.StepReturn;
+import mcts_spacegame.model_mcts.BackupModifer;
+import mcts_spacegame.models_mcts_nodes.NodeInterface;
+import mcts_spacegame.models_space.SpaceGrid;
+import mcts_spacegame.models_space.SpaceGridInterface;
+import mcts_spacegame.models_space.State;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestBackupModifer {
+
+    private static final double DELTA = 0.1;
+    SpaceGrid spaceGrid;
+    Environment environment;
+    BackupModifer bum;
+    List<List<StepReturn>> simulationResultsEmpty;
+
+    @Before
+    public void init() {
+        spaceGrid= SpaceGridInterface.new3times7Grid();
+        environment=new Environment(spaceGrid);
+
+        simulationResultsEmpty=new ArrayList<>(new ArrayList<>());
+
+    }
+
+    @Test
+    public void moveFromx0y0Tox6y2GivesTwoMoveCost() {
+        State pos=new State(0,0);
+        List<Action> actions= Arrays.asList(Action.up,Action.up,Action.still,Action.still,Action.still,Action.still,Action.still);
+        List<StepReturn> stepReturns = getReturnForActions(pos, actions);
+        List<NodeInterface> nodesFromRootToSelected=getNodes(actions,stepReturns);
+
+        stepReturns.forEach(System.out::println);
+        nodesFromRootToSelected.forEach(System.out::println);
+
+        bum=new BackupModifer(actions,nodesFromRootToSelected,stepReturns,simulationResultsEmpty);
+
+        bum.backup();
+
+        nodesFromRootToSelected.forEach(System.out::println);
+
+
+        //Assert.assertEquals(-Environment.MOVE_COST *2,G, DELTA);
+    }
+
+    private List<StepReturn> getReturnForActions(State pos, List<Action> actions) {
+        List<StepReturn> stepReturns=new ArrayList<>();
+        for (Action action: actions) {
+            System.out.println("pos = " + pos);
+            StepReturn stepReturn= environment.step(action, pos);
+            pos.setFromReturn(stepReturn);
+
+            stepReturns.add(stepReturn);
+
+            if (stepReturn.isTerminal) {
+                break;
+            }
+        }
+        return stepReturns;
+    }
+
+    private List<NodeInterface> getNodes(List<Action> actions, List<StepReturn> stepReturns) {
+
+        List<NodeInterface> nodes=new ArrayList<>();
+        for (int i = 0; i < actions.size() ; i++) {
+            StepReturn sr=stepReturns.get(i);
+            Action a=actions.get(i);
+
+            if (!sr.isTerminal) {
+                nodes.add(NodeInterface.newNotTerminal(sr.newPosition,a));
+            } else if (sr.isFail) {
+                nodes.add(NodeInterface.newTerminalFail(sr.newPosition,a));
+            } else {
+                nodes.add(NodeInterface.newTerminalNotFail(sr.newPosition,a));
+            }
+        }
+        return nodes;
+    }
+
+}
