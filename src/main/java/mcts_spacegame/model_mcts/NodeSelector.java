eba09b545f65e10b5dd1a6d4dcbbe7d7fc8bb415
Jonas Hellgren <jonas.hellgren@volvo.com>
2022-11-29 13:22:32 +0100

    strular

--- src/main/java/mcts_spacegame/model_mcts/NodeSelector.java
+++ src/main/java/mcts_spacegame/model_mcts/NodeSelector.java
@@ -2,6 +2,7 @@ package mcts_spacegame.model_mcts;
 
 import common.MathUtils;
 import lombok.Getter;
+import lombok.extern.java.Log;
 import mcts_spacegame.enums.Action;
 import mcts_spacegame.models_mcts_nodes.NodeInterface;
 import org.apache.commons.math3.util.Pair;
@@ -10,8 +11,10 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Getter
+@Log
 public class NodeSelector {
 
     private static final int C = 1;
@@ -30,23 +33,34 @@ public class NodeSelector {
         nodesFromRootToSelected.clear();
         NodeInterface currentNode=nodeRoot;
         nodesFromRootToSelected.add(currentNode);
-        while (currenNodeHasChilds(currentNode)) {
-            currentNode=selectChild(currentNode);
+        while (currentNodeHasNonTerminalChilds(currentNode)) {
+            if (selectChild(currentNode).isEmpty()) {
+                break;
+            } else {
+                log.warning("No valid node selected");
+                currentNode=selectChild(currentNode).get();  //todo not repear selectChild
+            }
+
             actionsFromRootToSelected.add(currentNode.getAction());
             nodesFromRootToSelected.add(currentNode);
         }
         return currentNode;
     }
 
-    private boolean currenNodeHasChilds(NodeInterface currentNode) {
-        return currentNode.nofChildNodes() != 0;
+    private boolean currentNodeHasNonTerminalChilds(NodeInterface currentNode) {
+        List<NodeInterface> nonTerminalNodes = getNonTerminalChildrenNodes(currentNode);
+        return nonTerminalNodes.size() > 0;
     }
 
-    public NodeInterface selectChild(NodeInterface node) {
-        List<Pair<NodeInterface,Double>> nodeUCTPairs=getListOfPairs(node);
+    public Optional<NodeInterface> selectChild(NodeInterface node) {
+        List<Pair<NodeInterface,Double>> nodeUCTPairs= getListOfPairsExcludeTerminalNodes(node);
       //  nodeUCTPairs.forEach(System.out::println);
         Optional<Pair<NodeInterface, Double>> pair = getPairWithHighestUct(nodeUCTPairs);
-        return pair.orElseThrow().getFirst();
+
+        return pair.isEmpty()
+                ? Optional.empty()
+                : Optional.ofNullable(pair.get().getFirst());
+
     }
 
     @NotNull
@@ -55,14 +69,24 @@ public class NodeSelector {
                 reduce((res, item) -> res.getSecond() > item.getSecond() ? res : item);
     }
 
-    private List<Pair<NodeInterface,Double>>  getListOfPairs(NodeInterface node) {
+    private List<Pair<NodeInterface,Double>> getListOfPairsExcludeTerminalNodes(NodeInterface node) {
         List<Pair<NodeInterface,Double>> nodeUCTPairs=new ArrayList<>();
-        for (NodeInterface childNode: node.getChildNodes() ) {
+        List<NodeInterface> nonTerminalNodes = getNonTerminalChildrenNodes(node);
+
+        for (NodeInterface childNode: nonTerminalNodes ) {
             Action actionToReachChildNode=childNode.getAction();
             double uct=calcUct(node,actionToReachChildNode);
             nodeUCTPairs.add(new Pair<>(childNode,uct));
         }
         return nodeUCTPairs;
+    }
+
+    @NotNull
+    private List<NodeInterface> getNonTerminalChildrenNodes(NodeInterface node) {
+        List<NodeInterface> nonTerminalNodes= node.getChildNodes().stream()
+                .filter(NodeInterface::isNotTerminal)
+                .collect(Collectors.toList());
+        return nonTerminalNodes;
     }
 
     private double calcUct(NodeInterface node, Action action) {
