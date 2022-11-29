e349cc60747952beb7a08084b75af16a1bb97626
Jonas Hellgren <jonas.hellgren@volvo.com>
2022-11-29 19:39:03 +0100

    bug getNonTestedActions fixad

--- src/main/java/mcts_spacegame/model_mcts/ActionSelector.java
+++ src/main/java/mcts_spacegame/model_mcts/ActionSelector.java
@@ -1,12 +1,13 @@
 package mcts_spacegame.model_mcts;
 
 import common.ConditionalUtils;
-import common.MathUtils;
 import lombok.extern.java.Log;
 import mcts_spacegame.enums.Action;
 import mcts_spacegame.models_mcts_nodes.NodeInterface;
 import org.apache.commons.lang3.RandomUtils;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,16 +21,21 @@ public class ActionSelector {
     }
 
     public Action select() {
-        List<Action> testedActions=nodeSelected.getChildNodes().stream().map(cn -> cn.getAction()).collect(Collectors.toList());
-        List<Action> nonTestedActions=Action.applicableActions();
-        nonTestedActions.removeAll(testedActions);
 
-        System.out.println("nodeSelected ActionSelector = " + nodeSelected);
-        System.out.println("nonTestedActions ActionSelector = " + nonTestedActions);
+        List<Action> nonTestedActions = getNonTestedActions();
         int nofNonTestedActions=nonTestedActions.size();
         ConditionalUtils.executeOnlyIfConditionIsTrue(nofNonTestedActions==0,
                 () -> log.warning("No non tested actions"));
         return nonTestedActions.get(RandomUtils.nextInt(0,nofNonTestedActions));
+    }
+
+    @NotNull
+    private List<Action> getNonTestedActions() {
+        List<Action> testedActions = nodeSelected.getChildNodes().stream()
+                .map(NodeInterface::getAction).collect(Collectors.toList());
+        List<Action> nonTestedActions=new ArrayList<>(Action.applicableActions());  //must be mutable
+        nonTestedActions.removeAll(testedActions);
+        return nonTestedActions;
     }
 
 }
