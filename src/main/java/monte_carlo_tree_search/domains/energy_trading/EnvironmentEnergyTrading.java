01fc9878da7ccdc89504f77e55c1350a78233490
Jonas Hellgren <jonas.hellgren@volvo.com>
2023-03-14 07:27:19 +0100

    step

--- src/main/java/monte_carlo_tree_search/domains/energy_trading/EnvironmentEnergyTrading.java
+++ src/main/java/monte_carlo_tree_search/domains/energy_trading/EnvironmentEnergyTrading.java
@@ -11,18 +11,20 @@ import java.util.Map;
 
 public class EnvironmentEnergyTrading implements EnvironmentGenericInterface<VariablesEnergyTrading, Integer> {
 
-
     public static final int MIN_TIME = 0;
     public static final int MAX_TIME = 7;
     public static final int TIME_STEP_IN_HOUR_DURATION = 3;
 
+    public static final double SOE_MIN = 0.2;
+    public static final double SOE_MAX = 0.8;
+    public static final double SOE_MIN_END = 0.5;
 
-    private static final List<Double> PRICE_DEFAULT= Arrays.asList(0.5,1d,1d,1d,1d,1d,1.5,1d);
+    private static final List<Double> PRICE_DEFAULT = Arrays.asList(0.5, 1d, 1d, 1d, 1d, 1d, 1.5, 1d);
     private static final Map<Integer, Double> ACTION_POWER_IN_KW_MAP = Map.of(
-            -2,-2d, -1,-1d,0,-0d, 1,1d,2,2d);
-    private static final Double BATTERY_ENERGY=30d;  //kWh
+            -2, -2d, -1, -1d, 0, -0d, 1, 1d, 2, 2d);
+    private static final Double BATTERY_ENERGY = 30d;  //kWh
 
-    private List<Double> priceVsTime;
+    private final List<Double> priceVsTime;
 
     public EnvironmentEnergyTrading(List<Double> priceVsTime) {
         this.priceVsTime = priceVsTime;
@@ -41,17 +43,37 @@ public class EnvironmentEnergyTrading implements EnvironmentGenericInterface<Var
     public StepReturnGeneric<VariablesEnergyTrading> step(ActionInterface<Integer> action,
                                                           StateInterface<VariablesEnergyTrading> state) {
 
+        int timePres = state.getVariables().time;
+        double pricePres = priceVsTime.get(timePres);
+        double soEPres = state.getVariables().SoE;
+        double powerPresent = ACTION_POWER_IN_KW_MAP.get(action.getValue());
+        int timeNew = timePres + 1;
+        double soENew = soEPres + powerPresent * (double) TIME_STEP_IN_HOUR_DURATION / BATTERY_ENERGY;
 
-        int timePres=state.getVariables().time;
-        double pricePres=priceVsTime.get(timePres);
-        double soEPres=state.getVariables().SoE;
+        StateInterface<VariablesEnergyTrading> stateNew = StateEnergyTrading.newFromVariables(
+                VariablesEnergyTrading.builder().time(timeNew).SoE(soENew).build());
 
-        double presentPower= ACTION_POWER_IN_KW_MAP.get(action.getValue());
+        return StepReturnGeneric.<VariablesEnergyTrading>builder()
+                .newState(stateNew)
+                .isFail(isFail(stateNew))
+                .isTerminal(isTerminal(stateNew))
+                .reward(pricePres * powerPresent * TIME_STEP_IN_HOUR_DURATION)
+                .build();
 
-        int timeNew=timePres+1;
-        double soENew= soEPres+presentPower*(double) TIME_STEP_IN_HOUR_DURATION/BATTERY_ENERGY;
-
-
-        return null;
     }
+
+    private boolean isFail(StateInterface<VariablesEnergyTrading> state) {
+        VariablesEnergyTrading vars = state.getVariables();
+        return  vars.SoE < SOE_MIN ||
+                vars.SoE > SOE_MAX ||
+                vars.time > MAX_TIME ||
+                vars.time == MAX_TIME && vars.SoE < SOE_MIN_END;
+    }
+
+    private boolean isTerminal(StateInterface<VariablesEnergyTrading> state) {
+        VariablesEnergyTrading vars = state.getVariables();
+        return vars.time == MAX_TIME;
+    }
+
+
 }
--- src/main/java/monte_carlo_tree_search/domains/energy_trading/StateEnergyTrading.java
+++ src/main/java/monte_carlo_tree_search/domains/energy_trading/StateEnergyTrading.java
@@ -3,21 +3,30 @@ package monte_carlo_tree_search.domains.energy_trading;
 import lombok.Getter;
 import monte_carlo_tree_search.classes.StepReturnGeneric;
 import monte_carlo_tree_search.interfaces.StateInterface;
+import org.jetbrains.annotations.NotNull;
+import java.util.function.Predicate;
 
 @Getter
 public class StateEnergyTrading implements StateInterface<VariablesEnergyTrading> {
 
-
     VariablesEnergyTrading variables;
+
+    static Predicate<Integer> isValidTime =
+            time -> time>=EnvironmentEnergyTrading.MIN_TIME &&
+            time <= EnvironmentEnergyTrading.MAX_TIME;
+    static Predicate<Double> isValidSoE= soe -> soe>=0 && soe <= 1;
+
 
     StateEnergyTrading(VariablesEnergyTrading variables) {
         this.variables = variables;
     }
 
     public static StateEnergyTrading newFromVariables(VariablesEnergyTrading variables) {
+        if (!isVariablesValid(variables)) {
+            throw new IllegalArgumentException(getErrorMessage(variables));
+        }
         return new StateEnergyTrading(variables);
     }
-
 
     @Override
     public StateInterface<VariablesEnergyTrading> copy() {
@@ -31,6 +40,17 @@ public class StateEnergyTrading implements StateInterface<VariablesEnergyTrading
 
     public String toString() {
         return variables.toString();
+    }
+
+    public static boolean isVariablesValid(VariablesEnergyTrading variables) {
+        return isValidTime.test(variables.time) &&
+                isValidSoE.test(variables.SoE);
+    }
+
+    @NotNull
+    private static String getErrorMessage(VariablesEnergyTrading variables) {
+        return "isValidTime = "+isValidTime.test(variables.time)+
+                ", isValidSoE = "+isValidSoE.test(variables.SoE);
     }
 
 }
