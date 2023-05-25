0012a1a509492dfc1ae01ad47893d05bd61e999b
Jonas Hellgren <jonas.hellgren@volvo.com>
2023-05-25 13:26:11 +0200

    låta PROB_RANDOM minska med episod

--- src/test/java/multi_step_td/TestNstepTD.java
+++ src/test/java/multi_step_td/TestNstepTD.java
@@ -2,9 +2,8 @@ package multi_step_td;
 
 import common.Counter;
 import common.ListUtils;
-import common.MathUtils;
+import common.ScalerLinear;
 import lombok.extern.java.Log;
-import monte_carlo_tree_search.helpers.TreeInfoHelper;
 import multi_step_temp_diff.helpers.AgentInfo;
 import multi_step_temp_diff.helpers.NStepTDHelper;
 import multi_step_temp_diff.interfaces.AgentInterface;
@@ -12,7 +11,6 @@ import multi_step_temp_diff.interfaces.EnvironmentInterface;
 import multi_step_temp_diff.models.AgentTabular;
 import multi_step_temp_diff.models.ForkEnvironment;
 import multi_step_temp_diff.models.StepReturn;
-import org.apache.arrow.flatbuf.Int;
 import org.apache.commons.math3.util.Pair;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,11 +18,18 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * https://www.cs.ubc.ca/labs/lci/mlrg/slides/Multi-step_Bootstrapping.pdf
+ * https://lcalem.github.io/blog/2018/11/19/sutton-chap07-nstep
+ */
+
 @Log
 public class TestNstepTD {
     private static final double DELTA = 0.1;
     private static final double PROB_RANDOM = 0.2;
     private static final int START_STATE = 0;
+    private static final int NOF_EPISODES = 100;
+    private static final double P_RAND_START = 0.25d,P_RAND_END = 0d;
     EnvironmentInterface environment;
     AgentInterface agent;
     AgentTabular agentCasted;
@@ -36,11 +41,11 @@ public class TestNstepTD {
         environment = new ForkEnvironment();
         agent = AgentTabular.newDefault();
         agentCasted = (AgentTabular) agent;       //to access class specific methods
-        int nofEpisodes = 1000;
+        int nofEpisodes = 100;
 
         helper = NStepTDHelper.builder()
                 .n(3)
-                .episodeCounter(new Counter(0, nofEpisodes))
+                .episodeCounter(new Counter(0, NOF_EPISODES))
                 .timeCounter(new Counter(0, Integer.MAX_VALUE))
                 .build();
     }
@@ -49,20 +54,15 @@ public class TestNstepTD {
     public void fewEpisodesThreeStepper() {
 
         NStepTDHelper h=helper;
+        ScalerLinear scaler=new ScalerLinear(0, NOF_EPISODES, P_RAND_START, P_RAND_END);
         while (!h.episodeCounter.isExceeded()) {
             agent.setState(START_STATE);
             h.reset();
             do {
                 if (h.timeCounter.getCount() < h.T) {
-                    final int action = agent.chooseAction(PROB_RANDOM);
+                    final int action = agent.chooseAction(scaler.calcOutDouble(h.episodeCounter.getCount()));
                     stepReturn = environment.step(agent.getState(), action);
-
-                    if (agent.getState()==5) {
-                        System.out.println("action = " + action);
-                        System.out.println("stepReturn = " + stepReturn);
-                        System.out.println("agentCasted.getPairs() = " + agentCasted.getPairs());
-                    }
-
+                    //print(action);
                     h.statesMap.put(h.timeCounter.getCount(),agent.getState());
                     agent.updateState(stepReturn);
                     h.timeReturnMap.put(h.timeCounter.getCount()+1, stepReturn);
@@ -80,7 +80,7 @@ public class TestNstepTD {
                 }
                 h.timeCounter.increase();
             } while (h.tau != h.T - 1);
-            System.out.println("h after episode = " + h);
+           // System.out.println("h after episode = " + h);
 
             h.episodeCounter.increase();
 
@@ -90,6 +90,14 @@ public class TestNstepTD {
         AgentInfo agentInfo=new AgentInfo(agent);
         System.out.println("stateValueMap = " + agentInfo.stateValueMap(environment.stateSet()));
 
+    }
+
+    private void print(int action) {
+        if (agent.getState()==5) {
+            System.out.println("action = " + action);
+            System.out.println("stepReturn = " + stepReturn);
+            System.out.println("agentCasted.getPairs() = " + agentCasted.getPairs());
+        }
     }
 
     private double ifNotBeyondTerminalBackUpFromStateAhead(NStepTDHelper h, double G) {
