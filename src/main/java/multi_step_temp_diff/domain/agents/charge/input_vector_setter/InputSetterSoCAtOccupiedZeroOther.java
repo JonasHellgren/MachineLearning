2bae94751c2b410dcbb85eaba1b07c49ee3ca22b
Jonas Hellgren <jonas.hellgren@ri.se>
2023-08-07 08:29:25 +0200

    TestInputSetterSoCAtOccupiedZeroOther

--- src/main/java/multi_step_temp_diff/domain/agents/charge/input_vector_setter/InputSetterSoCAtOccupiedZeroOther.java
+++ src/main/java/multi_step_temp_diff/domain/agents/charge/input_vector_setter/InputSetterSoCAtOccupiedZeroOther.java
@@ -1,19 +1,12 @@
 package multi_step_temp_diff.domain.agents.charge.input_vector_setter;
 
-import common.Conditionals;
 import lombok.NonNull;
-import multi_step_temp_diff.domain.agent_abstract.StateInterface;
 import multi_step_temp_diff.domain.agent_valueobj.AgentChargeNeuralSettings;
 import multi_step_temp_diff.domain.environment_valueobj.ChargeEnvironmentSettings;
 import multi_step_temp_diff.domain.environments.charge.ChargeState;
-import multi_step_temp_diff.domain.environments.charge.ChargeVariables;
-import multi_step_temp_diff.domain.environments.fork.ForkState;
-
 import java.util.Arrays;
 import java.util.Optional;
-
 import static common.Conditionals.executeIfTrue;
-import static java.lang.System.out;
 import static multi_step_temp_diff.domain.environments.charge.ChargeState.*;
 
 public class InputSetterSoCAtOccupiedZeroOther implements InputVectorSetterChargeInterface {
@@ -32,7 +25,6 @@ public class InputSetterSoCAtOccupiedZeroOther implements InputVectorSetterCharg
         double[] inArray = new double[agentSettings.nofStates()];
         Arrays.fill(inArray, 0);
 
-     //   ChargeState state=(ChargeState) state;
         Optional<Integer> mappedPosA=positionMapper.map(posA.apply(state));
         Optional<Integer> mappedPosB=positionMapper.map(posB.apply(state));
 
