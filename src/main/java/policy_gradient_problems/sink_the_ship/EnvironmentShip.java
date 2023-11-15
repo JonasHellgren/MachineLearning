c4e45702eeb9d8d5808d5421540d102b0e74b802
Jonas Hellgren <jonas.hellgren@ri.se>
2023-11-15 10:28:30 +0100

    EnvironmentShip

---
+++ src/main/java/policy_gradient_problems/sink_the_ship/EnvironmentShip.java
@@ -0,0 +1,45 @@
+package policy_gradient_problems.sink_the_ship;
+
+import common.RandUtils;
+import java.util.Map;
+
+
+public class EnvironmentShip {
+    public static final int NOF_STATES = 2;
+
+    final double ANGLE_MAX = Math.PI / 4,CONSTANCE_OF_GRAVITY = 9.81;
+    final double SPEED_PROJECTILE_MPS = 150.0, DEVIATION_MAX_METER = 20d;
+
+    final Map<Integer,Double> DISTANCE_TO_SHIP_MAP = Map.of(0,1000d, 1,2000d);
+    final double REWARD_HIT = 1, REWARD_MISS = 0;
+    final boolean IS_TERMINAL = false;
+
+    public StepReturnShip step(int state, double action) {
+        boolean isHit=isHitting(state,action);
+        double reward = getReward(isHit);
+        int stateNew=getStateNew(state, isHit);
+        return new StepReturnShip(stateNew, IS_TERMINAL,reward);
+    }
+
+    private  double getReward(boolean isHit) {
+        return isHit ? REWARD_HIT : REWARD_MISS;
+    }
+
+    private boolean isHitting(int state, double normalizedAngle) {
+        double angleInRadians=ANGLE_MAX*normalizedAngle;
+        double distanceProjectile=SPEED_PROJECTILE_MPS
+                *Math.cos(angleInRadians)*2*SPEED_PROJECTILE_MPS
+                *Math.sin(angleInRadians)/CONSTANCE_OF_GRAVITY;
+        double distanceToShip=DISTANCE_TO_SHIP_MAP.get(state);
+        double projectileShipDistanceDeviation=Math.abs(distanceToShip-distanceProjectile);
+        return projectileShipDistanceDeviation< DEVIATION_MAX_METER;
+    }
+
+    private int getStateNew(int state, boolean isHit) {
+        return isHit
+                ? RandUtils.getRandomIntNumber(0, NOF_STATES -1)
+                : state;
+    }
+
+
+}
---
+++ src/main/java/policy_gradient_problems/sink_the_ship/StepReturnShip.java
@@ -0,0 +1,4 @@
+package policy_gradient_problems.sink_the_ship;
+
+public record StepReturnShip(int state, boolean isTerminal, double reward) {
+}
