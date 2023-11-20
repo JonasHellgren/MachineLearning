bb2776093dd61822bd472b3516eacb7351f2de8d
Jonas Hellgren <jonas.hellgren@ri.se>
2023-11-20 14:13:37 +0100

    cleaner

--- src/main/java/policy_gradient_problems/short_corridor/TrainerWithBaselineSC.java
+++ src/main/java/policy_gradient_problems/short_corridor/TrainerWithBaselineSC.java
@@ -4,16 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import org.apache.commons.math3.linear.ArrayRealVector;
-import org.jetbrains.annotations.NotNull;
 import policy_gradient_problems.common.Experience;
 import policy_gradient_problems.common.TrainerParameters;
 import policy_gradient_problems.common.TrainingTracker;
 import policy_gradient_problems.helpers.ReturnCalculator;
 
+/**
+ * Explained in shortCorridor.md
+ */
+
 @Getter
 public class TrainerWithBaselineSC  extends TrainerAbstractSC {
 
-    ArrayRealVector wVector;   //for value function
+    ArrayRealVector wVector;   //value function parameters
 
     @Builder
     public TrainerWithBaselineSC(@NonNull EnvironmentSC environment,
@@ -23,10 +26,6 @@ public class TrainerWithBaselineSC  extends TrainerAbstractSC {
         wVector = getZeroVector();
     }
 
-    @NotNull
-    private static ArrayRealVector getZeroVector() {
-        return new ArrayRealVector(EnvironmentSC.SET_OBSERVABLE_STATES_NON_TERMINAL.size());
-    }
 
     public void train() {
         var returnCalculator=new ReturnCalculator();
@@ -38,18 +37,35 @@ public class TrainerWithBaselineSC  extends TrainerAbstractSC {
             for (Experience experience:experienceListWithReturns) {
                 var gradLogVector = agent.calcGradLogVector(experience.state(),experience.action());
                 double Gt = experience.value();
-                ArrayRealVector phiVector=getZeroVector();
-                phiVector.setEntry(experience.state(),1d);
-                double value=phiVector.dotProduct(wVector);
-                double oldWi=wVector.getEntry(experience.state());
-                double delta=Gt-value;
-                wVector.setEntry(experience.state(),oldWi+parameters.beta()*delta);
-
+                var phiVector= getZeroVectorWithSingleElementAsOne(experience.state());
+                double delta = calcDelta(Gt, phiVector);
+                updateWeightVector(experience, delta);
                 var changeInThetaVector = gradLogVector.mapMultiplyToSelf(parameters.learningRate() * delta);
                 agent.setThetaVector(agent.getThetaVector().add(changeInThetaVector));
             }
             updateTracker(ei);
         }
+    }
+
+    private double calcDelta(double Gt, ArrayRealVector phiVector) {
+        double value= phiVector.dotProduct(wVector);
+        return Gt -value;
+    }
+
+
+    private static ArrayRealVector getZeroVector() {
+        return new ArrayRealVector(EnvironmentSC.SET_OBSERVABLE_STATES_NON_TERMINAL.size());
+    }
+
+    private static ArrayRealVector getZeroVectorWithSingleElementAsOne(int index) {
+        var vector=new ArrayRealVector(EnvironmentSC.SET_OBSERVABLE_STATES_NON_TERMINAL.size());
+        vector.setEntry(index,1d);
+        return vector;
+    }
+
+    private void updateWeightVector(Experience experience, double delta) {
+        double oldWi=wVector.getEntry(experience.state());
+        wVector.setEntry(experience.state(),oldWi+parameters.beta()* delta);
     }
 
 }
