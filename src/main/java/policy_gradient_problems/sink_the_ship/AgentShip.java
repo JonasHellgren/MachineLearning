b5e9e23b1520275924d8e94b76e1a18372981a83
Jonas Hellgren <jonas.hellgren@ri.se>
2023-11-29 07:46:05 +0100

    givenState0_whenGradLogForActionMeanS1_thenCorrect

--- src/main/java/policy_gradient_problems/sink_the_ship/AgentShip.java
+++ src/main/java/policy_gradient_problems/sink_the_ship/AgentShip.java
@@ -2,11 +2,16 @@ package policy_gradient_problems.sink_the_ship;
 
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.util.Pair;
+import org.opencv.core.Mat;
+import policy_gradient_problems.short_corridor.EnvironmentSC;
 
 import java.util.Random;
+import java.util.function.Function;
 
 import static common.ArrayUtil.createArrayWithSameDoubleNumber;
+import static java.lang.System.arraycopy;
 import static org.apache.commons.lang3.ArrayUtils.subarray;
+import static policy_gradient_problems.common.GradLogCalculator.calculateGradLog;
 
 public class AgentShip {
 
@@ -38,11 +43,19 @@ public class AgentShip {
         return sampleFromNormDistribution(meanStdPair);
     }
 
-    /**
-     * https://www.javamex.com/tutorials/random_numbers/gaussian_distribution_2.shtml
-     */
-    private double sampleFromNormDistribution(Pair<Double, Double> meanStdPair) {
-        return meanStdPair.getFirst()+random.nextGaussian() * meanStdPair.getSecond();
+    public ArrayRealVector calcGradLogVector(int state, double action) {
+        return new ArrayRealVector(createGradLogAllStates(state, calculateGradLogForState(state,action)));
+    }
+
+    Function<Double,Double> sqr=(n) -> Math.pow(n,2);
+
+    private double[] calculateGradLogForState(int state, double action) {
+        var meanAndStd=getMeanAndStdFromThetaVector(state);
+        double mean=meanAndStd.getFirst();
+        double std=meanAndStd.getSecond();
+        double gradMean=1/sqr.apply(std)*(action-mean);
+        double gradStd=sqr.apply(action-mean)/sqr.apply(std)-1d;
+        return new double[]{gradMean,gradStd};
     }
 
     public Pair<Double,Double> getMeanAndStdFromThetaVector(int state) {
@@ -51,6 +64,21 @@ public class AgentShip {
         double mean = thetaVector.getEntry(indexFirstTheta);
         double std = Math.exp(thetaVector.getEntry(indexFirstTheta+NOF_THETAS_PER_STATE-1));
         return Pair.create(mean, std);
+    }
+
+    /**
+     * https://www.javamex.com/tutorials/random_numbers/gaussian_distribution_2.shtml
+     */
+    private double sampleFromNormDistribution(Pair<Double, Double> meanStdPair) {
+        return meanStdPair.getFirst()+random.nextGaussian() * meanStdPair.getSecond();
+    }
+
+    private static double[] createGradLogAllStates(int stateObserved, double[] gradLogForStateObserved) {
+        double[] gradLogAllStates = createArrayWithSameDoubleNumber(getThetaLength(), 0);
+        int indexSource = 0;
+        int indexFirstTheta = getIndexFirstThetaForState(stateObserved);
+        arraycopy(gradLogForStateObserved, indexSource, gradLogAllStates, indexFirstTheta, NOF_THETAS_PER_STATE);
+        return gradLogAllStates;
     }
 
     private static void throwIfBadState(int state) {
---
+++ src/test/java/policygradient/sink_the_ship/TestAgentShip.java
@@ -0,0 +1,103 @@
+package policygradient.sink_the_ship;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvSource;
+import policy_gradient_problems.sink_the_ship.AgentShip;
+import policy_gradient_problems.sink_the_ship.EnvironmentShip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestAgentShip {
+
+    public static final int PROB_DIRECT_TO_TERMINAL = 0;
+    public static final double DELTA = 0.1;
+    public static final double MEAN_S0 = 0.3, MEAN_S1 = 0.5, THETA_STD0 = -2,THETA_STD1 = 0;
+    public static final int NOF_SAMPLES = 100;
+    EnvironmentShip environment;
+    AgentShip agent;
+
+    @BeforeEach
+    public void init() {
+        environment = new EnvironmentShip();
+        agent = AgentShip.newWithRandomStartStateAndGivenThetas(new double[]{MEAN_S0, THETA_STD0, MEAN_S1, THETA_STD1});
+    }
+
+    @ParameterizedTest   //state, mean, std
+    @CsvSource({
+            "0, 0.3,0.1", "1, 0.5,1.0"
+    })
+    public void whenState_thenCorrectMeanAndStd(ArgumentsAccessor arguments) {
+        int s = arguments.getInteger(0);
+        double mean = arguments.getDouble(1);
+        double std = arguments.getDouble(2);
+
+        var msPair = agent.getMeanAndStdFromThetaVector(s);
+
+        System.out.println("msPair = " + msPair);
+
+        assertEquals(msPair.getFirst(), mean, DELTA);
+        assertEquals(msPair.getSecond(), std, DELTA);
+    }
+
+    @Test
+    public void givenState0_whenManySamples_thenCorrect() {
+        DescriptiveStatistics ds = getDescriptiveStatisticsForState(0);
+        assertEquals(ds.getMean(), MEAN_S0, DELTA);
+        assertEquals(ds.getStandardDeviation(), Math.exp(THETA_STD0), DELTA);
+    }
+
+    @Test
+    public void givenState1_whenManySamples_thenCorrect() {
+        DescriptiveStatistics ds = getDescriptiveStatisticsForState(1);
+        assertEquals(ds.getMean(), MEAN_S1, DELTA);
+        assertEquals(ds.getStandardDeviation(), Math.exp(THETA_STD1), DELTA);
+    }
+
+    @ParameterizedTest   //action, derMeanDes, derStdDes  (sign(derX) is used)
+    @CsvSource({
+            "0, 0,-1",
+            "0.1, 1,-1",
+            "-0.1, -1,-1",
+            "2, 1,1"
+    })
+    public void givenState0_whenGradLogForActionMeanS1_thenCorrect(ArgumentsAccessor arguments) {
+        int state = 0;
+
+        double deltaAction = arguments.getDouble(0);
+        double action=MEAN_S0+deltaAction;
+        double derMeanDes = arguments.getDouble(1);
+        double derStdDes = arguments.getDouble(2);
+
+        var gradLog0=agent.calcGradLogVector(state,action);
+        double derMean=gradLog0.getEntry(0);
+        double derStd=gradLog0.getEntry(1);
+
+        assertEquals(Math.signum(derMeanDes), Math.signum(derMean));
+        assertEquals(Math.signum(derStdDes), Math.signum(derStd));
+    }
+
+
+
+    private DescriptiveStatistics getDescriptiveStatisticsForState(int state) {
+        List<Double> actions=new ArrayList<>();
+        for (int i = 0; i < NOF_SAMPLES; i++) {
+            actions.add(agent.chooseAction(state));
+        }
+        DescriptiveStatistics ds=new DescriptiveStatistics();
+        actions.forEach(ds::addValue);
+        return ds;
+    }
+
+
+
+
+}
