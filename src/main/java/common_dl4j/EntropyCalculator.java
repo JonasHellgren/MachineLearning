e1661997320b7df045342ac02ad694b214f1e5b5
Jonas Hellgren <jonas.hellgren@ri.se>
2024-01-02 13:33:01 +0100

    TestEntropyCalculator

--- src/main/java/common_dl4j/EntropyCalculator.java
+++ src/main/java/common_dl4j/EntropyCalculator.java
@@ -5,14 +5,26 @@ import org.nd4j.linalg.ops.transforms.Transforms;
 
 public class EntropyCalculator {
 
+    public static final double EPS = 1e-10;
+
     public static double calcCrossEntropy(INDArray realProb, INDArray estProb) {
-        INDArray logP = Transforms.log(estProb);
+        return calcCrossEntropy(realProb,estProb, EPS);
+    }
+
+    public static double calcCrossEntropy(INDArray realProb, INDArray estProb, double eps) {
+        INDArray adjustedProbToAvoidLogZero = estProb.add(eps);
+        INDArray logP = Transforms.log(adjustedProbToAvoidLogZero);
         INDArray vec= realProb.mul(logP);
         return  -vec.sumNumber().doubleValue();
     }
 
     public static double calcEntropy(INDArray estProb) {
-        INDArray invP = estProb.rdiv(1.0);
+        return calcEntropy(estProb,EPS);
+    }
+
+    public static double calcEntropy(INDArray estProb, double eps) {
+        INDArray adjustedProbToAvoidDivZero = estProb.add(eps);
+        INDArray invP = adjustedProbToAvoidDivZero.rdiv(1.0);
         INDArray surprise = Transforms.log(invP);
         INDArray vec= estProb.mul(surprise);
         return  vec.sumNumber().doubleValue();
